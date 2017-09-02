1ca1b23a49139e74befe0d64abe6a190d7320fe2
waroon <58160638@go.buu.ac.th>
2017-09-02 15:38:19 +0700

    When playerA get First score and playerB no score

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,24 +1,35 @@
 function TennisGame(){
+  const scoreString = ['Love','Fifteen','Thirty','Forthy']
+  this.playerAScore = 0
+  //this.playerBScore = 0
   this.reset = () => {
-
+    this.playerAScore = 0
   }
-
   this.echo = () => {
-    return 'Love - Love'
+     return `${scoreString[this.playerAScore]} - Love`
+  }
+  this.playerAGetScore = () => {
+    this.playerAScore++
   }
 }
 test('Echo "Love - Love " When press reset button', () => {
     //Arrange
-
       let app = new TennisGame()
-
     //Act
       app.reset()
       let result = app.echo()
-
-
     //Assert
-    expect(result).toBe('Love - Love')
+      expect(result).toBe('Love - Love')
+})
 
-
+test('Echo "Fifteen - Love" When playerA get First score and playerB no score', () => {
+    //Arrange
+      //let app = new ButtonA()
+      let app = new TennisGame()
+      app.reset()
+      app.playerAGetScore()
+    //Act
+      let result = app.echo()
+    //Assert
+      expect(result).toBe('Fifteen - Love')
 })
