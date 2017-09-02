f0da2ea1b8c4a37b4940ec40f833d765ba4cb8fd
waroon <58160638@go.buu.ac.th>
2017-09-02 15:57:26 +0700

    When playerA get two score and playerB get Three score

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,15 +1,19 @@
 function TennisGame(){
   const scoreString = ['Love','Fifteen','Thirty','Forthy']
   this.playerAScore = 0
-  //this.playerBScore = 0
+  this.playerBScore = 0
   this.reset = () => {
     this.playerAScore = 0
+    this.playerBScore = 0
   }
   this.echo = () => {
-     return `${scoreString[this.playerAScore]} - Love`
+     return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
   }
   this.playerAGetScore = () => {
     this.playerAScore++
+  }
+  this.playerBGetScore = () => {
+    this.playerBScore++
   }
 }
 test('Echo "Love - Love " When press reset button', () => {
@@ -32,4 +36,29 @@ test('Echo "Fifteen - Love" When playerA get First score and playerB no score', 
       let result = app.echo()
     //Assert
       expect(result).toBe('Fifteen - Love')
+})
+
+test('Echo "Love - Fifteen" When playerA no score and playerB get Frist score', () => {
+    //Arrange
+      let app = new TennisGame()
+      app.reset()
+      app.playerBGetScore()
+    //Act
+      let result = app.echo()
+    //Assert
+      expect(result).toBe('Love - Fifteen')
+})
+test('Echo "Thirty - Forthy" When playerA get two score and playerB get Three score', () => {
+    //Arrange
+      let app = new TennisGame()
+      while(app.playerAScore != 2){
+        app.playerAGetScore()
+      }
+      while(app.playerBScore != 3){
+        app.playerBGetScore()
+      }
+    //Act
+      let result = app.echo()
+    //Assert
+      expect(result).toBe('Thirty - Forthy')
 })
